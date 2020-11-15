56f3f606d52166515392f8fc9f94a059c9fc351d
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2020-11-16 00:45:38 +0100

    Add default button classes

--- muse-for-anything-ui/src/resources/attributes/classes-for.ts
+++ muse-for-anything-ui/src/resources/attributes/classes-for.ts
@@ -2,6 +2,7 @@ import { autoinject } from "aurelia-framework";
 
 const DEFAULT_CLASSES = {
     "CARD": ["shadow-4", "pa1", "br2", "mb2"],
+    "BUTTON-FLAT": ["pa1", "br1", "ma1", "hover-bg-near-white", "hover-shadow"],
 };
 
 @autoinject()
@@ -12,6 +13,8 @@ export class ClassesForCustomAttribute {
         const classList = DEFAULT_CLASSES[newValue.toUpperCase()];
         if (classList != null) {
             this.element.classList.add(...classList);
+        } else {
+            console.error(`Could not find classes for ${newValue} (${newValue.toUpperCase()})`);
         }
     }
 }
