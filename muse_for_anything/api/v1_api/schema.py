c038459c0f382cce3d7f433e5f8966a77ae79411
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2020-11-24 00:16:27 +0100

    Add more metadata to api schemas returned by the api
    Fix api schemas using the incorrect python names for properties

--- muse_for_anything/api/v1_api/schema.py
+++ muse_for_anything/api/v1_api/schema.py
@@ -1,24 +1,56 @@
 """Module containing the schema API of the v1 API."""
 
-from muse_for_anything.api.v1_api.models.schema import (
-    SchemaApiObject,
-    SchemaApiObjectSchema,
-)
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, cast
 from flask import request, jsonify, Response
 from flask.helpers import url_for
 from flask.views import MethodView
 from flask_babel import gettext
 from flask_smorest import abort
+from marshmallow.base import SchemaABC
+from marshmallow.fields import Field
 from http import HTTPStatus
 
 from .root import API_V1
 from ..util import JSON_SCHEMA
 from ..base_models import ApiLink, ApiResponse, BaseApiObject, DynamicApiResponseSchema
 from .models.ontology import NamespaceSchema
+from .models.schema import (
+    SchemaApiObject,
+    SchemaApiObjectSchema,
+)
+
+
+def create_schema_from_model(
+    model: SchemaABC, **kwargs: Dict[str, Any]
+) -> Dict[str, Any]:
+    key: str
+    field: Field
+    for key, field in model.fields.items():
+        if field.data_key:
+            field.metadata.setdefault("name", field.data_key)
+    schema = cast(Dict[str, Any], JSON_SCHEMA.dump(model))
+    defs: Optional[Dict[str, Any]] = schema.get("definitions")
+    if defs is None:
+        return schema
+    for schema_name, extra in kwargs.items():
+        sub_schema: Optional[Dict[str, Any]] = defs.get(schema_name)
+        if sub_schema is None:
+            raise ValueError(
+                f"The schema {schema_name} is not defined in the schema of the model {model}!"
+            )
+        for key, value in extra.items():
+            sub_schema[key] = value
+    return schema
+
 
 SCHEMAS: Dict[str, Dict[str, Any]] = {
-    "Namespace": JSON_SCHEMA.dump(NamespaceSchema(exclude=("self",)))
+    "Namespace": create_schema_from_model(
+        NamespaceSchema(exclude=("self",)),
+        NamespaceSchema={
+            "propertyOrder": {"name": 10, "description": 20},
+            "hiddenProperties": ["createdOn", "updatedOn", "deletedOn"],
+        },
+    )
 }
 
 
