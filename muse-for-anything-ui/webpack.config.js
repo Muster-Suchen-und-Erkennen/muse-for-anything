1e2720951770019ad0e9252efaee3ec6b365016e
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2021-03-23 21:37:30 +0100

    Fix wrong base url in webpack config

--- muse-for-anything-ui/webpack.config.js
+++ muse-for-anything-ui/webpack.config.js
@@ -16,7 +16,7 @@ const when = (condition, config, negativeConfig) =>
 // primary config:
 const outDir = path.resolve(__dirname, project.platform.output);
 const srcDir = path.resolve(__dirname, 'src');
-const baseUrl = '/static';
+const baseUrl = '/static/';
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
     content: [
