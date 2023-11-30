d666621f08eab19d55537319f9469febd4fabd7f
flyinglzx <lzx19970109@gmail.com>
2023-12-01 00:18:42 +0100

    Map complex attributes of objects in OWL

--- muse_for_anything/db/models/owl.py
+++ muse_for_anything/db/models/owl.py
@@ -1,5 +1,5 @@
 from muse_for_anything.db.models.namespace import Namespace
-from muse_for_anything.db.models.taxonomies import Taxonomy
+from muse_for_anything.db.models.taxonomies import Taxonomy, TaxonomyItem
 from muse_for_anything.db.models.ontology_objects import OntologyObject
 from muse_for_anything.db.models.ontology_objects import OntologyObjectType
 
@@ -56,9 +56,40 @@ class OWL:
             'description_of_the_namespace': namespace.description,
             'taxonomy_list': taxonomies,
             'type_list': ontology_object_types,
-            'object_list': ontology_objects
+            'object_list': [(object, self.get_ontology_object_variables(object)) for object in ontology_objects]
         }
 
         rendered_template = template.render(data)
-        return rendered_template    
+        return rendered_template 
+
+    def get_ontology_object_variables(self, ontology_object: OntologyObject):
+        current = ontology_object.current_version
+        data = current.data
+        type = current.ontology_type_version
+        root_schema = type.root_schema
+        result = []
+        if 'referenceType' in root_schema and 'referenceKey' in root_schema:
+            ref_name = self.find_reference_name(data['referenceType'], data['referenceKey'])
+            result.append({'name': ref_name, 'ref': ref_name})
+        else:
+            for name,value in data.items():
+                property_schema = root_schema['properties'][name]
+                if 'referenceType' in property_schema and 'referenceKey' in property_schema:
+                    ref_name = self.find_reference_name(value['referenceType'], value['referenceKey'])
+                    result.append({'name': name, 'ref': ref_name})
+                else:
+                    result.append({'name': name, 'value': value})
+        return result
+    
+    def find_reference_name(self, reference_type, reference_key):
+        if reference_type == 'ont-object':
+            return OntologyObject.query.filter(
+                OntologyObject.id == int(reference_key['objectId']),
+            ).first().name
+        elif reference_type == 'ont-taxonomy-item':
+            return TaxonomyItem.query.filter(
+                TaxonomyItem.id == int(reference_key['taxonomyItemId']),
+            ).first().name
+        else:
+            raise Exception('Unknown reference type')  
     
