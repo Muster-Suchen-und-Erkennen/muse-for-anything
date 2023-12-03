546a31e18477d30ee191f5b99855759bafcdba29
flyinglzx <lzx19970109@gmail.com>
2023-12-04 00:47:43 +0100

    Add property constraints

--- muse_for_anything/db/models/owl.py
+++ muse_for_anything/db/models/owl.py
@@ -9,7 +9,7 @@ import os
 class OWL:
 
     def map_namespace_to_owl(self, namespace: Namespace):
-        # TODO implement mapping
+        # implement mapping
         # for each attribute in namespace, create owl ontology namespace attribute
         # parse owl namespace in xml format
         # return owl namespace (in XML format)
@@ -53,7 +53,7 @@ class OWL:
 
         data = {
             'name': namespace.name,
-            'description_of_the_namespace': namespace.description,
+            'description': namespace.description,
             'taxonomy_list': taxonomies,
             'type_list': ontology_object_types,
             'object_list': [(object, self.get_ontology_object_variables(object)) for object in ontology_objects]
