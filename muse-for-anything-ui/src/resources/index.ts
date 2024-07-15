c651fceaac1d74834c82909760a30688f9210a7c
Fabian Bühler <fabian.buehler@iaas.uni-stuttgart.de>
2024-07-15 12:21:46 +0200

    Add value converter to find items in arrays

--- muse-for-anything-ui/src/resources/index.ts
+++ muse-for-anything-ui/src/resources/index.ts
@@ -3,6 +3,7 @@ import { PLATFORM } from "aurelia-pal";
 
 export function configure(config: FrameworkConfiguration): void {
     config.globalResources([
+        PLATFORM.moduleName("resources/value-converters/find"),
         PLATFORM.moduleName("resources/value-converters/json"),
         PLATFORM.moduleName("resources/value-converters/number"),
         PLATFORM.moduleName("resources/binding-behaviors/optional"),
