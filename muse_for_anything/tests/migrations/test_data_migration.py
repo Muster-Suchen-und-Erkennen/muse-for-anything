60ede7ea768a6f3f90f342c4233bbba39e4e108a
Jan Weber <st157366@stud.uni-stuttgart.de>
2024-09-30 15:36:25 +0200

    Add test from array to string

--- muse_for_anything/tests/migrations/test_data_migration.py
+++ muse_for_anything/tests/migrations/test_data_migration.py
@@ -738,13 +738,126 @@ class TestMigrationToString(unittest.TestCase):
         pass
 
     def test_from_array_to_str(self):
-        pass
+        source_schema = """
+            {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": false,
+            "definitions": {
+                "root": {
+                    "arrayType": "array",
+                    "items": {
+                        "type": [
+                            "boolean"
+                        ]
+                    },
+                    "type": [
+                        "array"
+                    ]
+                }
+            },
+            "title": "Type"
+            }
+            """
+        data_object = """
+        {
+            "data": {
+                "createdOn": "2024-09-27T08:02:44.203024",
+                "data": [
+                    2,
+                    9,
+                    44
+                ],
+                "deletedOn": null,
+                "description": "",
+                "name": "Object",
+                "self": {
+                    "href": "http://localhost:5000/api/v1/namespaces/1/objects/13/",
+                    "name": "IntObject",
+                    "rel": [],
+                    "resourceKey": {
+                        "namespaceId": "1",
+                        "objectId": "13"
+                    },
+                    "resourceType": "ont-object",
+                    "schema": "http://localhost:5000/api/v1/schemas/ontology/27/"
+                }
+            },
+            "updatedOn": "2024-09-27T08:02:44.213790",
+            "version": 1
+        }
+        """
+        transformations = match_schema(source_schema, self.target_schema)
+        updated_data_object_true = migrate_object(
+            data_object, self.target_schema, transformations
+        )
+        self.assertEqual("[2, 9, 44]", updated_data_object_true["data"]["data"])
+        self.assertEqual(2, updated_data_object_true["version"])
+
 
     def test_from_obj_to_str(self):
         pass
 
     def test_from_tuple_to_str(self):
-        pass
+        source_schema = """
+            {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": false,
+            "definitions": {
+                "root": {
+                    "arrayType": "tuple",
+                    "items": [
+                        {
+                        "type": ["boolean"]
+                        },
+                        {
+                        "type": ["integer"]
+                        },
+                        {
+                        "type": ["string"]
+                        }
+                    ],
+                    "type": ["array"]
+                    }
+            },
+            "title": "Type"
+            }
+            """
+        data_object = """
+        {
+            "data": {
+                "createdOn": "2024-09-27T08:02:44.203024",
+                "data": [
+                    true,
+                    12,
+                    "Hello"
+                ],
+                "deletedOn": null,
+                "description": "",
+                "name": "Object",
+                "self": {
+                    "href": "http://localhost:5000/api/v1/namespaces/1/objects/13/",
+                    "name": "IntObject",
+                    "rel": [],
+                    "resourceKey": {
+                        "namespaceId": "1",
+                        "objectId": "13"
+                    },
+                    "resourceType": "ont-object",
+                    "schema": "http://localhost:5000/api/v1/schemas/ontology/27/"
+                }
+            },
+            "updatedOn": "2024-09-27T08:02:44.213790",
+            "version": 1
+        }
+        """
+        transformations = match_schema(source_schema, self.target_schema)
+        updated_data_object_true = migrate_object(
+            data_object, self.target_schema, transformations
+        )
+        self.assertEqual("[True, 12, \'Hello\']", updated_data_object_true["data"]["data"])
+        self.assertEqual(2, updated_data_object_true["version"])        
 
     def test_from_res_ref_to_str(self):
         pass
