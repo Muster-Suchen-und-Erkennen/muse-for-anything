eeb2521f6101655b7b41f1e965ab5e143450c1e7
Jan Weber <st157366@stud.uni-stuttgart.de>
2024-10-17 11:38:27 +0200

    Add tests for extract type

---
+++ muse_for_anything/tests/migrations/test_extract_type.py
@@ -0,0 +1,140 @@
+from muse_for_anything.json_migrations.jsonschema_matcher import *
+import unittest
+
+
+class TestTypeExtraction(unittest.TestCase):
+
+    def test_extract_string(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {
+                "root": {
+                    "arrayType": "array",
+                    "items": {"type": ["integer"]},
+                    "type": ["array"],
+                }
+            },
+            "title": "Type",
+        }
+        self.assertEqual("array", extract_type(schema))
+
+    def test_extract_boolean(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {"root": {"type": ["boolean"]}},
+            "title": "Type",
+        }
+        self.assertEqual("boolean", extract_type(schema))
+
+    def test_extract_enum(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {"root": {"enum": ["all", True, 1234.56, None]}},
+            "title": "Type",
+        }
+        self.assertEqual("enum", extract_type(schema))
+
+    def test_extract_integer(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {"root": {"type": ["integer"]}},
+            "title": "Type",
+        }
+        self.assertEqual("integer", extract_type(schema))
+
+    def test_extract_number(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {"root": {"type": ["number"]}},
+            "title": "Type",
+        }
+        self.assertEqual("number", extract_type(schema))
+
+    def test_extract_object(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {
+                "root": {
+                    "properties": {
+                        "one": {"type": ["integer"]},
+                        "three": {"type": ["boolean"]},
+                        "two": {"type": ["string"]},
+                    },
+                    "type": ["object"],
+                }
+            },
+            "title": "Type",
+        }
+        self.assertEqual("object", extract_type(schema))
+
+    def test_extract_resource_reference(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {
+                "root": {"customType": "resourceReference", "type": ["object"]}
+            },
+            "title": "Type",
+        }
+        self.assertEqual("resourceReference", extract_type(schema))
+
+    def test_extract_schema_ref(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {
+                "root": {
+                    "$ref": "http://localhost:5000/api/v1/namespaces/4/types/8/versions/1/#/definitions/root"
+                }
+            },
+            "title": "Type",
+        }
+        self.assertEqual("schemaReference", extract_type(schema))
+
+    def test_extract_string(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {"root": {"type": ["string"]}},
+            "title": "Type",
+        }
+        self.assertEqual("string", extract_type(schema))
+
+    def test_extract_tuple(self):
+        schema = {
+            "$ref": "#/definitions/root",
+            "$schema": "http://json-schema.org/draft-07/schema#",
+            "abstract": False,
+            "definitions": {
+                "root": {
+                    "arrayType": "tuple",
+                    "items": [
+                        {"type": ["boolean"]},
+                        {"type": ["integer"]},
+                        {"type": ["string"]},
+                    ],
+                    "type": ["array"],
+                }
+            },
+            "title": "Type",
+        }
+        self.assertEqual("tuple", extract_type(schema))
+
+
+if __name__ == "__main__":
+    unittest.main()
